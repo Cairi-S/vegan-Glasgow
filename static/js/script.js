cb9cf5b8d78521568c1e38fc1d54caaded092745
Cairi-S <cairi.swainson@gmail.com>
2021-06-19 13:11:44 +0000

    script:All code moved to python, file emptied

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+const flashPrompt = document.querySelector(".flash-prompts")
 const password = document.querySelector("#password");
 const confirmPassword = document.querySelector("#confirmPassword");
 const formBtn = document.querySelector(".form-button");
@@ -6,7 +7,7 @@ formBtn.addEventListener("click", checkPasswords);
 
 function checkPasswords() {
   if(password.value != confirmPassword.value){
-      alert("Passwords must match");
+      flashPrompt.innerHTML("Passwords must match");
       return false;
   }
   else if(password.value == confirmPassword.value){
